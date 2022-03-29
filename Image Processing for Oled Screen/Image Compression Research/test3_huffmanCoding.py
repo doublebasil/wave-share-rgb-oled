aadfd0006776ff4da851d1776965f9fa1ff420f3
Thomas Flaherty <tom.flaherty03.com>
2022-03-29 20:19:00 +0100

    huffman

--- Image Processing for Oled Screen/Image Compression Research/test3_huffmanCoding.py
+++ Image Processing for Oled Screen/Image Compression Research/test3_huffmanCoding.py
@@ -3,6 +3,13 @@ from math import pow
 
 """
 Attempting to use huffman coding to compress images
+
+Thoughts:
+    in c I could implement the huffman table with pointers and 1x2 arrays
+    in python I could implement the table with 1x2 lists within list
+
+    Making the table is gonna be a brainf
+
 """
 
 def rgbTo16Bit(rgb):
@@ -13,12 +20,18 @@ def main():
     
     rgbData = list(im.getdata())
 
-
+    
 
 
 
 
 if __name__ == '__main__':
     # main()
-    print(rgbTo16Bit((31, 63, 31)))
-    print(pow(2, 16) - 1)
+
+    # print(rgbTo16Bit((31, 63, 31)))
+    # print(pow(2, 16) - 1)
+
+    # This does sort by only the first column, which is good
+    a = [[2, 'b'], [3, 'c'], [1, 'a'], [0, 'j']]
+    a.sort()
+    print(a)
